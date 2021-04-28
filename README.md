b0ad2ec98d8c9e3962fa875da79f3ede88ea8cab
Pierre Miniggio <pierre.miniggio@gmail.com>
2021-04-28 21:47:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Install using composer :
+```
+composer require pierreminiggio/github-user-agent
+```
---
+++ src/GithubUserAgent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PierreMiniggio\GithubUserAgent;
+
+class GithubUserAgent
+{
+    public const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 OPR/74.0.3911.160';
+}
